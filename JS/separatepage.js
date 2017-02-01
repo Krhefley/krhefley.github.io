6aa3fc34e2ba841c0b497e1c2f252c1d1a27b2b5
Krhefley <krhefley@gmail.com>
2017-02-01 13:04:26 -0700

    new js

---
+++ JS/separatepage.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+$(window).resize(function() {
+  var more = document.getElementById("js-navigation-more");
+  if ($(more).length > 0) {
+    var windowWidth = $(window).width();
+    var moreLeftSideToPageLeftSide = $(more).offset().left;
+    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+
+    if (moreLeftSideToPageRightSide < 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
+    }
+
+    if (moreLeftSideToPageRightSide > 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
+    }
+  }
+});
+
+$(document).ready(function() {
+  var menuToggle = $("#js-mobile-menu").unbind();
+  $("#myMenu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#myMenu").slideToggle(function(){
+      if($("#myMenu").is(":hidden")) {
+        $("#myMenu").removeAttr("style");
+      }
+    });
+  });
+}); 
+});
